a0d5afbd1883262643a3080490ff6747c23cbfee
jawc-05 <joaoalfredowc@gmail.com>
2026-08-17 15:07:02 -0300

    feat: adding basic structure for java and models pkg

---
+++ Exercises/src/main/java/br/com/jawc/models/NoLUE.java
@@ -0,0 +1,15 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.models;
+
+
+public class NoLUE {
+    public int info;
+    public NoLUE prox;
+
+    public NoLUE(int info) {
+        this.info = info;
+        this.prox = null;
+    }
+}
