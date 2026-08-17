0716b55bc11570d9f531a8f92a4fa2f32b41f845
jawc-05 <joaoalfredowc@gmail.com>
2026-08-17 15:09:15 -0300

    feat: added NoPolinomio model in the models pkg

---
+++ Exercises/src/main/java/br/com/jawc/models/NoPolinomio.java
@@ -0,0 +1,18 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.models;
+
+
+public class NoPolinomio {
+
+    public double a;
+    public int i;
+    public NoPolinomio eloa;
+    public NoPolinomio elop;
+
+    public NoPolinomio(double a, int i) {
+        this.a = a;
+        this.i = i;
+    }
+}
