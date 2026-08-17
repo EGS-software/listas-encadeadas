be49421f843765b8f475619b34f65343ad040dda
jawc-05 <joaoalfredowc@gmail.com>
2026-08-17 15:08:24 -0300

    feat: adding NoLDE model in the models pkg

---
+++ Exercises/src/main/java/br/com/jawc/models/NoLDE.java
@@ -0,0 +1,15 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.models;
+
+
+public class NoLDE {
+    public int info;
+    public NoLDE ant;
+    public NoLDE prox;
+
+    public NoLDE(int info) {
+        this.info = info;
+    }
+}
